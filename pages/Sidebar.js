02acff0732e459023306fb416d23309c12d5a8d7
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-08-12 13:20:56 +0530

    added one to one chat users

--- pages/Sidebar.js
+++ pages/Sidebar.js
@@ -5,9 +5,17 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import ChatIcon from '@material-ui/icons/Chat';
 import SearchIcon from '@material-ui/icons/Search';
 import * as EmailValidator from "email-validator";
-import { auth } from "../firebase"
+import { auth, db } from "../firebase"
+import { useAuthState } from "react-firebase-hooks/auth"
+import {useCollection} from "react-firebase-hooks/firestore"
+import Chat from "./Chat"
 
 export default function Sidebar() {
+
+    const [user] = useAuthState(auth);
+    const userChatRef = db.collection('chats').where('users','array-contains', user.email);
+
+    const [chatsSnapshot] = useCollection(userChatRef)
 
     const createChat = () => {
         const input = prompt(
@@ -16,11 +24,16 @@ export default function Sidebar() {
         
         if(!input) return null;
 
-        if(EmailValidator.validate(input)) {
-            
+        if(EmailValidator.validate(input) && !chatAlreadyPresent(input)  && input !== user.email) {
+            db.collection('chats').add({
+                users: [user.email, input],
+            })
         }
-
     }
+
+    const chatAlreadyPresent = (recipientEmail) =>  !!chatsSnapshot?.docs.find(chat => chat.data().users.find(user => user === recipientEmail)?.length > 0 )
+
+    
 
     return (
         <Container>
@@ -42,6 +55,9 @@ export default function Sidebar() {
 
             <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
             {/* List of chats */}
+            {chatsSnapshot?.docs.map(chat => {
+                return <Chat key={chat.id} id={chat.id} users={chat.data().users}/>
+            })}
         </Container>
     )
 }
