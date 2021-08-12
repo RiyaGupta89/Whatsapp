5d0728b440df6b37270f70c90a47fc680cea65f8
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-08-13 05:02:30 +0530

    Implemented sending messages

--- pages/ChatScreen.js
+++ pages/ChatScreen.js
@@ -4,77 +4,168 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
 import { useRouter } from "next/router";
 import { Avatar, IconButton } from "@material-ui/core";
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import AttachFileIcon from '@material-ui/icons/AttachFile';
-import { useCollection } from "react-firebase-hooks/firestore"
-import { db } from "../firebase"
-
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import Message from "./Message";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+import { useState } from "react"
+import firebase from "firebase"
+import getRecipientEmail from "../utils/getRecipientEmail"
 
 function ChatScreen({ chat, messages }) {
+  console.log(chat)
+  console.log(messages)
   const [user] = useAuthState(auth);
+  const [input, setInput] = useState("")
   const router = useRouter();
 
+  const [messagesSnapshot] = useCollection(
+    db
+      .collection("chats")
+      .doc(router.query.id)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+  );
+
+  const [recipientSnapshot] = useCollection(db.collection('users').where('email', '==', getRecipientEmail(chat.users, user)))
+
   const showMessages = () => {
-    
+    if (messagesSnapshot) {
+      return messagesSnapshot.docs.map((message) => (
+        <Message
+          key={message.id}
+          user={message.data().user}
+          message={{
+            ...message.data(),
+            timestamp: message.data().timestamp?.toDate().getTime(),
+          }}
+        />
+      ));
+    }
+    else {
+      return JSON.parse(messages).map(message => (
+          <Message key={message.id} user={message.user} message={message} />
+      ))
+    }
+  };
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    db.collection("users").doc(user.uid).set({
+      lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+    },{merge: true})
+
+
+    db.collection('chats').doc(router.query.id).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      user: user.email,
+      photoURL: user.photoURL,
+    })
+
+    setInput('')
   }
+
+  const recipient = recipientSnapshot?.docs?.[0]?.data();
+  const recipientEmail = getRecipientEmail(chat.users, user);
 
   return (
     <Container>
       <Header>
-        <Avatar />
-      <HeaderInformation>
-        <h3>Recipient Email</h3>
-        <p>LAST SEEN ...</p>
-      </HeaderInformation>
-      <HeaderIcons>
-        <IconButton>
-          <AttachFileIcon />
-        </IconButton>
-        <IconButton>
-          <MoreVertIcon />
+      {recipient?
+        <Avatar src={recipient?.photoURL}/>
+        :
+        <Avatar> {recipientEmail[0].toUpperCase()} </Avatar>
+      }
+        <HeaderInformation>
+          <h3>{recipientEmail}</h3>
+          <p>LAST SEEN ...</p>
+        </HeaderInformation>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFileIcon />
           </IconButton>
-      </HeaderIcons>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </HeaderIcons>
       </Header>
 
       <MessageContainer>
-
-          <EndMessage />
+        {showMessages()}
+        <EndMessage />
       </MessageContainer>
+      <InputContainer>
+        <InsertEmoticonIcon />
+        <Input value={input} onChange={(e)=> {setInput(e.target.value)}}/>
+        <button hidden disabled={!input} onClick={sendMessage} type="submit" >Send Message</button>
+        <MicIcon />
+      </InputContainer>
     </Container>
   );
 }
 
 export default ChatScreen;
 
-const Container = styled.div``;
+const Input = styled.input`
+  flex: 1;
+  align-items: center;
+  background-color: whitesmoke;
+  outline: 0;
+  border: none;
+  border-radius: 10px;
+  padding: 20px;
+  margin-left: 15px;
+  margin-right: 15px;
+`;
+
+const Container = styled.div`
+
+`;
 
 const Header = styled.div`
-    position: sticky;
-    background-color: white;
-    z-index: 100;
-    top: 0;
-    display: flex;
-    padding: 11px;
-    height: 80px;
-    align-items: center;
-    border-bottom: 1px solid whitesmoke;
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
 `;
 
 const HeaderInformation = styled.div`
-    margin-left: 15px;
-    flex: 1;
-    >h3 {
-        margin-bottom: 3px;
-    }
-    >p {
-        font-size: 14px;
-        color: gray;
-    }
+  margin-left: 15px;
+  flex: 1;
+  > h3 {
+    margin-bottom: 3px;
+  }
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
 `;
 
 const HeaderIcons = styled.div``;
 
-const MessageContainer = styled.div``;
+const MessageContainer = styled.div`
+  padding: 30px;
+  background-color: #e5ded8;
+  min-height: 90vh;
+`;
 
 const EndMessage = styled.div``;
 
+const InputContainer = styled.form`
+display: flex;
+align-items: center;
+padding: 10px;
+position: sticky;
+bottom: 0;
+background-color: white;
+z-index: 100;
+`;
--- pages/Sidebar.js
+++ pages/Sidebar.js
@@ -1,116 +1,134 @@
-import React from 'react'
-import styled from "styled-components"
-import { Avatar, IconButton, Button } from '@material-ui/core';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import ChatIcon from '@material-ui/icons/Chat';
-import SearchIcon from '@material-ui/icons/Search';
+import React from "react";
+import styled from "styled-components";
+import { Avatar, IconButton, Button } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChatIcon from "@material-ui/icons/Chat";
+import SearchIcon from "@material-ui/icons/Search";
 import * as EmailValidator from "email-validator";
-import { auth, db } from "../firebase"
-import { useAuthState } from "react-firebase-hooks/auth"
-import {useCollection} from "react-firebase-hooks/firestore"
-import Chat from "./Chat"
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+import Chat from "./Chat";
 
 export default function Sidebar() {
+  const [user] = useAuthState(auth);
+  const userChatRef = db
+    .collection("chats")
+    .where("users", "array-contains", user.email);
 
-    const [user] = useAuthState(auth);
-    const userChatRef = db.collection('chats').where('users','array-contains', user.email);
+  const [chatsSnapshot] = useCollection(userChatRef);
 
-    const [chatsSnapshot] = useCollection(userChatRef)
+  const createChat = () => {
+    const input = prompt(
+      "Please enter an email address for the user you wish to chat with"
+    );
 
-    const createChat = () => {
-        const input = prompt(
-            "Please enter an email address for the user you wish to chat with"
-            );
-        
-        if(!input) return null;
+    if (!input) return null;
 
-        if(EmailValidator.validate(input) && !chatAlreadyPresent(input)  && input !== user.email) {
-            db.collection('chats').add({
-                users: [user.email, input],
-            })
-        }
+    if (
+      EmailValidator.validate(input) &&
+      !chatAlreadyPresent(input) &&
+      input !== user.email
+    ) {
+      db.collection("chats").add({
+        users: [user.email, input],
+      });
     }
+  };
 
-    const chatAlreadyPresent = (recipientEmail) =>  !!chatsSnapshot?.docs.find(chat => chat.data().users.find(user => user === recipientEmail)?.length > 0 )
+  const chatAlreadyPresent = (recipientEmail) =>
+    !!chatsSnapshot?.docs.find(
+      (chat) =>
+        chat.data().users.find((user) => user === recipientEmail)?.length > 0
+    );
 
-    
+  return (
+    <Container>
+      <Header>
+        <UserAvatar
+          src={user.photoURL}
+          onClick={() => {
+            auth.signOut();
+          }}
+        />
+        <IconContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconContainer>
+      </Header>
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
 
-    return (
-        <Container>
-            <Header>
-                <UserAvatar src={user.photoURL} onClick={()=> {auth.signOut()}}/>
-                <IconContainer>
-                <IconButton>
-                    <ChatIcon />
-                </IconButton>
-                <IconButton>
-                    <MoreVertIcon />
-                </IconButton>
-                </IconContainer>
-            </Header>
-            <Search>
-                <SearchIcon />
-                <SearchInput placeholder="Search in chats"/>
-            </Search>
-
-            <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
-            {/* List of chats */}
-            {chatsSnapshot?.docs.map(chat => {
-                return <Chat key={chat.id} id={chat.id} users={chat.data().users}/>
-            })}
-        </Container>
-    )
+      <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
+      {/* List of chats */}
+      {chatsSnapshot?.docs.map((chat) => {
+        return <Chat key={chat.id} id={chat.id} users={chat.data().users} />;
+      })}
+    </Container>
+  );
 }
 
 const Container = styled.div`
-   
+    flex: 0.45;
+    border-right: 1px solid whitesmoke;
+    height: 100vh;
+    min-width: 300px;
+    max-width: 350px;
+    overflow-y: scroll;
+
+    ::-webkit-scrollbar {
+        display: none;
+    }
+
+    -ms-overflow-style: none;
+    scrollbar-width: none;
 `;
 
 const Header = styled.div`
-       display: flex;
-       position: sticky;
-       top: 0;
-       background-color: white;
-       z-index: 100;
-       justify-content: space-between;
-       align-items: center;
-       padding: 15px;
-       height: 80px;
-       border-bottom: 1px solid whitesmoke;
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 100;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
 `;
 
 const UserAvatar = styled(Avatar)`
-
-    cursor: pointer;
-    :hover { 
-        opacity: 0.8;
-    }
-
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
 `;
 
-const IconContainer = styled.div`
-
-`;
+const IconContainer = styled.div``;
 
 const Search = styled.div`
-    display: flex;
-    padding: 20px;
-    align-items: center;
-    border-radius: 2px;
+  display: flex;
+  padding: 20px;
+  align-items: center;
+  border-radius: 2px;
 `;
 
 const SearchInput = styled.input`
-    flex: 1;
-    outline-width: 0;
-    border: none;
+  flex: 1;
+  outline-width: 0;
+  border: none;
 `;
 
 const SidebarButton = styled(Button)`
-    width: 100%;
-    &&& {
-        border-top: 1px solid whitesmoke;
-        border-bottom: 1px solid whitesmoke;
-    }
+  width: 100%;
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
 `;
-
-
