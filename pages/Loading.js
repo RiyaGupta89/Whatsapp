5ffb0f4ce85b4ca1c9035cce31bc873ba825f344
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-08-13 14:13:47 +0530

    Corrected image tag

--- pages/Loading.js
+++ pages/Loading.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Circle} from "better-react-spinkit"
+import Image from "next/image"
 
 export default function Loading() {
     return (
         <center style={{display: "grid", height: "100vh", placeItems: "center"}}>
         <div>
-            <img src="/logo.png" alt="logo" height={200} style={{ marginBottom: 10 }}/>
+            <Image src="/logo.png" alt="logo" height={200} width={200} style={{ marginBottom: 10 }}/>
             <Circle color="#3CBC28" size={60}/>
         </div>
         </center>
